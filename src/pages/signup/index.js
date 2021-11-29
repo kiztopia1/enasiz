043480c3c00a6cef03ae1c4ab0c7bcf36eba90f2
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-11-29 07:03:58 -0700

    input validation updated

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -10,7 +10,7 @@ class Signup extends React.Component {
     handelSubmit(event){
         event.preventDefault();
         const data = new FormData(event.target)
-        const username = data.get('username');
+        const username = data.get('username').trim();
         const password1 = data.get('password');
         const password2 = data.get('password2');
         if (password1 === password2){
@@ -23,23 +23,24 @@ class Signup extends React.Component {
     render () {
         return ( 
             <div className='main'>
-
+                
                 <div className="cont">
-                {this.state.error}
+                
                 <h2>Sign up</h2>
                 <form onSubmit={this.handelSubmit}>
                     <div className="input-cont">
                         <label htmlFor="username">user name</label>
-                        <input type="text" name="username" id="username" />
+                        <input type="text" name="username" id="username" required='true' />
                     </div>
                     <div className="input-cont">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="password" />
+                        <input type="password" name="password" id="password" required='true'/>
                     </div>
                     <div className="input-cont">
                         <label htmlFor="password2">confirm password</label>
-                        <input type='password' name='password2' id="password2"/>
+                        <input type='password' name='password2' id="password2" required='true'/>
                     </div>
+                    <p className="error">{this.state.error}</p>
                     
                     <button type="submit">Sign up</button>
                 </form>

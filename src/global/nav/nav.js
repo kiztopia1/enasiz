5086913db872665e4b7bb0ab84404275076c02b4
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-11-30 10:15:40 -0700

    state added for nav

--- src/global/nav/nav.js
+++ src/global/nav/nav.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { Link } from 'react-router-dom'
 import './index.css'
 
 class Nav extends React.Component {
-
+    constructor(){
+        this.state ={
+            status:false
+        };
+    }
     render() {
         return (
             <nav>

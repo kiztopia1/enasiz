adf97d0cc87f26d51b1c8c45a533f8921f6e793f
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-11-30 11:50:33 -0700

    create token btn logic added

--- src/pages/home/component/CreateToken.js
+++ src/pages/home/component/CreateToken.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './CreateToken.css'
 function CreateToken() {
+    const [Amount, setAmount] = useState(0)
+    const [AmountStatus, setAmountStatus] = useState(false)
+
+    let createToggler = () => {
+        setAmountStatus(!AmountStatus)
+    }
+
     return (
         <div className='create-token'>
             <div className="create">
-            <span className="create-btn">Creat Token</span>
+                <span className="create-btn" onClick={createToggler}>Creat Token</span>
+                <div className={"form " + (AmountStatus? ' show' : ' hide')}>
+                    <label htmlFor="amount">add amount</label>
+                    <input type="text" name="amount" id="amount" /> <span> birr</span>
+                </div>
             </div>
-
             <div className="connect">
             <span className="connect-btn">Connect</span>
             </div>

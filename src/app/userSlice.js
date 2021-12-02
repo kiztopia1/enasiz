568ee280db6e1de6fcd043b0cccd9e44c6700a50
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-12-02 13:39:20 -0700

    userSlice created to store user data

---
+++ src/app/userSlice.js
@@ -0,0 +1,24 @@
+import {  createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: null
+};
+
+export const counterSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+      login: (state, action) => {
+
+        state.user = action.payload
+      },
+      logout: (state) => {
+        state.user = null
+      },
+    }
+  });
+
+  export const { login, logout } = counterSlice.actions;
+  export const selectUser = (state) => state.user;
+  
+  export default counterSlice.reducer;

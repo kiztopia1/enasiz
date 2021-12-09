20aaa783251745feea7f9b9a4a788e46a1802b7d
kiztopia1 <62418642+kiztopia1@users.noreply.github.com>
2021-12-08 20:28:57 -0700

    user schema updated

--- backend/schema/user.js
+++ backend/schema/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: String,
     password:String,
-    tokens:  [{ type: mongoose.Schema.ObjectId, ref: "Token" }]
+    tokens:  [{ type: mongoose.Schema.ObjectId, ref: "Token" }],
+    balance: String
 });
 module.exports = mongoose.model('User', userSchema);

93db65c39b85a2b12146ad590fe9973760106386
kirubel <kiztopia.2020@gmail.com>
2021-12-19 11:25:43 +0300

    setStatus array added to tokens sechema to display status

--- backend/schema/token.js
+++ backend/schema/token.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const tokenSchema = new mongoose.Schema({
     name: String,
     amount:Number,
-    users: [],
+    users: Array,
+    setStatus: Array,
     status: String
 });
 module.exports = mongoose.model('Token', tokenSchema);
